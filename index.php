87e00614257b7beb7b10d9c0cffe4110b3396175
Atamaniv <atamaniv@gmail.com>
2023-08-27 10:05:23 +0300

    php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body>
-    xxx2
+<body style="background:#aaa;">
+    Працює :)____
 </body>
 </html>
