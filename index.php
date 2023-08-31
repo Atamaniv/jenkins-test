694b21406271163eb802c752adc52a3d02581854
Atamaniv <atamaniv@gmail.com>
2023-08-31 22:18:46 +0300

    Xenogerion

--- index.php
+++ index.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="uk">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
 <body style="background:#aaa;">
-    Працює :)____<br/> 
+<div class="container">
+   <h1> Xenogerion </h1>
+</div>
+
 <?php
-$env = parse_ini_file('.env');
+/*$env = parse_ini_file('.env');
 $DB_SERVER = $env["DB_SERVER"];
 $DB = $env["DB"];
 $DB_USER = $env["DB_USER"];
@@ -26,8 +30,8 @@ foreach ($conn->query($sql) as $row) {
     print $row['productname'] . "<br/>";
 }
 echo "Connected successfully";
-mysqli_close($conn);
+mysqli_close($conn);*/
 ?>    
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 </body>
 </html>
